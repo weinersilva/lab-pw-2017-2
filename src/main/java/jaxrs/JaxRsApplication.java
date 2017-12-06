71c918438aacbd6e929c886c69e3a705e09a74a7
Walison Moreira <walison.moreira@gmail.com>
2017-12-06 01:42:22 -0200

    Refatoração.

--- src/main/java/jaxrs/JaxRsApplication.java
+++ src/main/java/jaxrs/JaxRsApplication.java
@@ -1,10 +1,12 @@
-package professor;
+package jaxrs;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+import professor.ProfessorRest;
 
 @ApplicationPath("/api")
 public class JaxRsApplication extends Application {
